ba0b78e5cd21fd8117ebfb9f06cd6097e35694c0
adithravindran0fficial <adithravindranofficial@gmail.com>
2024-11-01 18:50:49 +0530

    Add project files.

---
+++ trianlge/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace trianlge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for(int i = 1; i <= n; i++)
+            {
+                for(int j = 1; j <= n; j++)
+                {
+                    if (j <= i)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
